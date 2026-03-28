7efa65f9c91ce52cb04d95365168feb6edf148a8
Leo Xia <lmxia9810@gmail.com>
2026-03-28 14:14:32 -0400

    Fixed getVelocity Method

--- Assets/Scripts/ShipMotor.cs
+++ Assets/Scripts/ShipMotor.cs
@@ -54,7 +54,7 @@ public class ShipMotor : MonoBehaviour
 
     public int getVelocity()
     {
-        return rb.velocity;
+        return rb.velocity.magnitude;
     }
 
     private void FixedUpdate()
